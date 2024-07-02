75c52da6e17a5d32c3d04b8893754951cb3d32c1
Vivekanand <vivek@voxglobaltech.com>
2024-07-02 10:21:18 +0530

    react-hook form config fixes

--- src/components/Input.js
+++ src/components/Input.js
@@ -14,9 +14,9 @@ function Input({ fieldName, fieldType, register, errors, validation }) {
         id={fieldName}
         name={fieldName}
         className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-blue-200"
-        {...register(fieldName, { validation })}
+        {...register(fieldName, { ...validation })}
       />
-      {errors.fieldName && <span>This field is required</span>}
+      {errors[fieldName] && <span>This field is required</span>}
     </div>
   );
 }
